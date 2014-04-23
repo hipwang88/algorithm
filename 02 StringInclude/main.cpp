edb3fa339dfda73b3d254b5935d8c1ea9975a140
hipwang <hipwang88@gmail.com>
2014-04-23 13:04:34 +0800

    quick sort
    判断一个字符串内是否含有另外的字符串，先对两个字符串进行快排，然后进行O(m+n)时间复杂度的遍历比较。

--- 02 StringInclude/main.cpp
+++ 02 StringInclude/main.cpp
@@ -3,15 +3,58 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int partition(string &strValue,int lo,int hi)
+{
+	char key = strValue[hi];
+	int index = lo-1;
+	
+	for (int i = lo; i < hi; ++i)
+	{
+		if (key >= strValue[i])
+		{
+			++index;
+			swap(strValue[index],strValue[i]);
+		}
+	}
+	swap(strValue[index+1],strValue[hi]);
+	
+	return index+1;
+}
+
+void quicksort(string &strValue,int lo,int hi)
+{
+	if (lo < hi)
+	{
+		int k = partition(strValue,lo,hi);
+		quicksort(strValue,lo,k-1);
+		quicksort(strValue,k+1,hi);
+	}
+}
+
 bool IsStringInclude(string strA,string strB)
 {
+	int posA = 0,posB = 0;
 	int nLenA = strA.length();
 	int nLenB = strB.length();
 	
 	if (nLenA < nLenB)
 		return false;
 	
-	return true;
+	while (posA < nLenA && posB < nLenB)
+	{
+		while (strA[posA] < strB[posB] && posA < nLenA-1)
+			posA++;
+			
+		if (strA[posA] != strB[posB])
+			break;
+			
+		posB++;
+	}
+	
+	if (posB == nLenB)
+		return true;
+	else
+		return false;
 }
 
 int main(int argc, char** argv) 
@@ -20,6 +63,8 @@ int main(int argc, char** argv)
 	
 	cin >> strA >> strB;
 	
+	quicksort(strA,0,strA.length()-1);
+	quicksort(strB,0,strB.length()-1);
 	cout << IsStringInclude(strA,strB) << endl;
 	
 	return 0;

769f30c4d0d65d4c3ffd9364181825dfe2d14481
hipwang <hipwang88@gmail.com>
2014-04-25 16:53:49 +0800

    find count of 1, completed!!!
    while (n)
    {
    n &= (n-1);
    ncount++;
    }

--- 05 查找一个Byte中1的个数/main.cpp
+++ 05 查找一个Byte中1的个数/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) 
 {
+	srand((int)time(NULL));
+	int num = rand();
+	int nCount = 0;
+	
+	cout << "Byte: 0x" << setbase(16) << num << " Count(1) = ";
+	
+	/*
+		00100000 -1 = 00011111
+		00100000 & 00011111 = 0
+	*/
+	while (num)
+	{
+		num &= (num-1);
+		++nCount;
+	}
+	
+	cout << nCount << endl;
 	
 	return 0;
 }
